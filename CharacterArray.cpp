dc6b8c8808f03be40c5bc940587ead4a43c1dac4
Grecia Bueno <77705265+Gigi-Pons@users.noreply.github.com>
2022-06-20 23:21:29 -0500

    Add files via upload

---
+++ CharacterArray.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+int linearSearch(int nums[], int n, int searchNum);
+
+int main()
+{   
+    int n;
+    cout << "Enter how many numbers: ";
+    cin >> n;
+    int nums[n];
+
+    cout << "\nEnter the numbers: ";
+    for(int i=0; i<n; i++)
+        cin >> nums[i];
+        
+    cout << "\nEnter a number to search for: ";
+    int searchNum;
+    cin >> searchNum;
+
+    int posNum = linearSearch(nums, n, searchNum);
+    if(posNum == -1)
+        cout << "\nNumber not found";
+    else   
+        cout << "\nNumber found at position " << posNum;
+
+
+    return 0;
+}
+
+int linearSearch(int nums[], int n, int searchNum)
+{
+    int posNum = -1;
+
+    for(int i=0; i<n; i++)
+    {
+        if(nums[i] == searchNum)
+        {
+            posNum = i;
+            break;
+        }
+    }
+
+    return posNum;
+}
+
